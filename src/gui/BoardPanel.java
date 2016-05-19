15e98dd74c0b409077be7a86e315c270af5f5b10
Paweł Malina <sruba96@gmail.com>
2016-05-19 03:33:45 +0200

    Rules - working properly :)

--- src/gui/BoardPanel.java
+++ src/gui/BoardPanel.java
@@ -1,12 +1,17 @@
 package gui;
 
 import data.Data;
+import domain.Germ;
+import logic.Rules;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.Transient;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by pawel on 08.05.16.
@@ -23,15 +28,22 @@ public class BoardPanel extends JPanel {
     private int width = PANELSIZE;
     private int height = PANELSIZE;
     private BoardMouseListener boardMouseListener;
+    private List<Germ> germList;
+    private Random random = new Random();
+    private Rules rules;
 
     public BoardPanel() {
+        germList = new ArrayList<Germ>();
+        Germ germ = new Germ(Color.DARK_GRAY);
+        germList.add(0, germ);
+        rules = new Rules();
         boardMouseListener = new BoardMouseListener(this);
         this.addMouseListener(boardMouseListener);
         timer = new Timer(500, new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-//                updateGrid();
+                grid = rules.generationUP(grid,true);
                 repaint();
             }
         });
@@ -45,8 +57,15 @@ public class BoardPanel extends JPanel {
         repaint();
     }
 
-    public void addLife(int x, int y){
-        grid[x][y] = 1;
+    public void addLife(int x, int y) {
+
+        Color c = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        Germ germ = new Germ(c);
+
+
+        grid[x][y] = germ.getLocalID();
+        germList.add(germ.getLocalID(), germ);
+
         repaint();
     }
 
@@ -64,8 +83,8 @@ public class BoardPanel extends JPanel {
         g.drawString("Pokolenie: " + generationCounter++, 0, 10);
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) {
-                    g.setColor(Color.red);
+                if (grid[i][j] > 0) {
+                    g.setColor(germList.get(grid[i][j]).getColor());
                     g.fillRect(j * CELLSIZE, i * CELLSIZE, CELLSIZE, CELLSIZE);
                 }
             }
